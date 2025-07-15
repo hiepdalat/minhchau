0f96d1d67074460219e051514ef4a31ddad4178f
hiepdalat <xuanhiep1112@gmail.com>
2025-07-15 12:02:50 +0700

    Update script-nhap.js

--- QuanLyCongNo/public/script-nhap.js
+++ QuanLyCongNo/public/script-nhap.js
@@ -24,6 +24,10 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     let currentReceiptItems = []; // Array to hold items for the current receipt being built
 
+    document.getElementById("btnBatDauNhap").addEventListener("click", () => {
+  const formNhap = document.getElementById("formNhapHang");
+  formNhap.classList.remove("hidden");
+});
     // Hàm loại bỏ dấu tiếng Việt
     function removeVietnameseTones(str) {
         return str.normalize("NFD")
